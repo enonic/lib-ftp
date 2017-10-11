cba867834b0b93fe7778d63577a90127096e1ba1
Christian Westgaard <cwe@enonic.com>
2017-10-11 12:11:13 +0200

    jdk.nashorn.internal.runtime.ECMAException and logging not ready yet.

--- test/src/test/resources/app/main-test.js
+++ test/src/test/resources/app/main-test.js
@@ -3,14 +3,19 @@ var Ftp = require('/lib/enonic/ftp');
 
 t.test('test ftp.getNames()', function () {
 
-    var ftp = new Ftp({
-        host: 'speedtest.tele2.net',
-        username: 'anonymous',
-        password: 'anonymous@example.com'
-    }).connect().login();
-    var names = ftp.getNames();
-    ftp.retrieveFile({ remote: '/1KB.zip' });
-    ftp.logout().disconnect();
-    t.assertEquals(names, 'Not Equal!');
-    //t.assertEquals('Equal', 'Equal');
+    try {
+        var ftp = new Ftp({
+            host: 'speedtest.tele2.net',
+            username: 'anonymous',
+            password: 'anonymous@example.com'
+        }); // Gives jdk.nashorn.internal.runtime.ECMAException
+        ftp.connect().login();
+        var names = ftp.getNames();
+        ftp.retrieveFile({ remote: '/1KB.zip' });
+        ftp.logout().disconnect();
+        t.assertEquals(names, 'Not Equal!');
+    } catch (e) {
+        log.error(e.message); // This log message doesn't go anywhere yet.
+        t.assertEquals('Equal', 'Not equal');
+    }
 });
